6557a9680f7cc4ee58d61750130d3f47398018e5
cmnorrisii <cmnorrisjr@gmail.com>
2023-04-03 18:37:38 -0700

    adds validations for model and requests and returns 422 error responses

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,4 @@
 class Beer < ApplicationRecord
+    validates :name, :brewer, :beer_type, :description, :image, presence:true
+    validates :description, length: { minimum: 10 }
 end
--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Beer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    beer = Beer.create(name: 'West Sixth Cerveza',
+    brewer: 'West Sixth Brewing',
+    beer_type: 'Mexican Lager',
+    description: "Oh, have you met me?",
+    image: "https://media.istockphoto.com/id/1332320975/vector/opened-beer-bottle-isolated-on-white[…]12&w=0&k=20&c=smq3qrWlQ5qmkZk1I3c1fAdM89BCi5vor9QYYAMW8jQ=")
+    expect(beer).to be_valid
+  end
+  it 'is invalid without a name' do
+    beer = Beer.create(brewer: 'West Sixth Brewing',
+    beer_type: 'Mexican Lager',
+    description: "Oh, have you met me?",
+    image: "https://media.istockphoto.com/id/1332320975/vector/opened-beer-bottle-isolated-on-white[…]12&w=0&k=20&c=smq3qrWlQ5qmkZk1I3c1fAdM89BCi5vor9QYYAMW8jQ=")
+    expect(beer.errors[:name]).to_not be_empty
+  end
+  it 'is invalid without a brewer' do
+    beer = Beer.create(name: 'West Sixth Cerveza',
+    beer_type: 'Mexican Lager',
+    description: "Oh, have you met me?",
+    image: "https://media.istockphoto.com/id/1332320975/vector/opened-beer-bottle-isolated-on-white[…]12&w=0&k=20&c=smq3qrWlQ5qmkZk1I3c1fAdM89BCi5vor9QYYAMW8jQ=")
+    expect(beer.errors[:brewer]).to_not be_empty
+  end
+  it 'is invalid without a beer_type' do
+    beer = Beer.create(name: 'West Sixth Cerveza',
+    brewer: 'West Sixth Brewing',
+    description: "Oh, have you met me?",
+    image: "https://media.istockphoto.com/id/1332320975/vector/opened-beer-bottle-isolated-on-white[…]12&w=0&k=20&c=smq3qrWlQ5qmkZk1I3c1fAdM89BCi5vor9QYYAMW8jQ=")
+    expect(beer.errors[:beer_type]).to_not be_empty
+  end
+  it 'is invalid without a description' do
+    beer = Beer.create(name: 'West Sixth Cerveza',
+    brewer: 'West Sixth Brewing',
+    beer_type: 'Mexican Lager',
+    image: "https://media.istockphoto.com/id/1332320975/vector/opened-beer-bottle-isolated-on-white[…]12&w=0&k=20&c=smq3qrWlQ5qmkZk1I3c1fAdM89BCi5vor9QYYAMW8jQ=")
+    expect(beer.errors[:description]).to_not be_empty
+  end
+  it 'is invalid without an image' do
+    beer = Beer.create(name: 'West Sixth Cerveza',
+    brewer: 'West Sixth Brewing',
+    beer_type: 'Mexican Lager',
+    description: "Oh, have you met me?")
+    expect(beer.errors[:image]).to_not be_empty
+  end
+  it 'is invalid if the description is less than 10 characters' do
+    beer = Beer.create
+    beer = Beer.create(name: 'West Sixth Cerveza',
+    brewer: 'West Sixth Brewing',
+    beer_type: 'Mexican Lager',
+    description: "Oh",
+    image: "https://media.istockphoto.com/id/1332320975/vector/opened-beer-bottle-isolated-on-white[…]12&w=0&k=20&c=smq3qrWlQ5qmkZk1I3c1fAdM89BCi5vor9QYYAMW8jQ=")
+    expect(beer.errors[:description]).to_not be_empty
+  end
 end
